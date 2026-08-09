9dc3b8c04ab41a2d4f5d33496dd37dfc694c70ce
zaditprodakwah <dakuprodakwah@gmail.com>
2026-08-09 18:24:19 +0700

    fix: noise.png 404 and enforce DATABASE_URL existence

--- apps/web/src/lib/prisma.ts
+++ apps/web/src/lib/prisma.ts
@@ -6,7 +6,10 @@ import ws from "ws";
 
 neonConfig.webSocketConstructor = ws;
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+  throw new Error("DATABASE_URL is missing in environment variables.");
+}
 const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool as any);
 
